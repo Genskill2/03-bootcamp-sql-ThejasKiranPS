8b03fbe66467c84a054fd608a0e9f3ecd274bf98
thejaskiranps <thejaskiranps@gmail.com>
2021-06-19 01:10:48 +0530

    fix q4

--- query4.sql
+++ query4.sql
@@ -1,1 +1,1 @@
-select s.name from subjects as s where s.id in (select bs.subject from books_subjects as bs where bs.book=(select b.id from books as b where name= "Atomic Habits"));
+select s.name from subjects as s where s.id in (select bs.subject from books_subjects as bs where bs.book=(select b.id from books as b where b.title= "Atomic Habits"));
